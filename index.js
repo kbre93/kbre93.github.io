71666513b03ee756b8e09ecf7e01b3b5e915db37
Kieran Brennan <kbre93@gmail.com>
2017-01-12 22:13:47 +1300

    Added projects page

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-function changeContent(category) {
-	//TODO: Make separate html files that are inserted into the tab content
-	if (category == 'About') {
-		document.getElementById('contentId').innerHTML = 'I am a Biomedical Engineering PhD Student from New Zealand working on Needle-free drug delivery';
-	} 
-	else if(category == 'Research') {
-		document.getElementById('contentId').innerHTML = 'Testing2!';	
-	}
-	else {
-		document.getElementById('contentId').innerHTML = 'Testing3!';
-	}
+// function changeContent(category) {
+// 	//TODO: Make separate html files that are inserted into the tab content
+// 	if (category == 'About') {
+// 		document.getElementById('contentId').innerHTML = 'I am a Biomedical Engineering PhD Student from New Zealand working on Needle-free drug delivery';
+// 	} 
+// 	else if(category == 'Research') {
+// 		document.getElementById('contentId').innerHTML = 'Testing2!';	
+// 	}
+// 	else {
+// 		document.getElementById('contentId').innerHTML = 'Testing3!';
+// 	}
+// }
 
+// $(document).ready(function() {
+// 	$('.categoryCont div.category').click(function() { // When clicked
+// 		var toLoad = $(this).attr('href');  			// Getting the content from the link to load
+// 		$('#contentId').hide('fast',loadContent); 		// Hiding the current content then loading new
 
-}
+// 		function loadContent() {
+// 			$('#contentId').load(toLoad,'',showNewContent);
+// 		};
 
+// 		function showNewContent() {
+// 			$('#contentId').show('normal');
+// 		}
+// 		return false;
+// 	});
+// });
 
-$(document).ready(function() {
-	
+$(document).ready( function() {
+    $(".tab").on("click", function() {
+        $("#contentId").load($(this).attr('href'));
+    });
 
-	$('.categoryCont div .category').click(function() {
-
-		var toLoad = $(this).attr('href');  // Getting the content from the link to load
-		$('#contentId').hide('fast',loadContent); 		// Hiding the current content then loading new
-
-		function loadContent() {
-			$('#contentId').load(toLoad,'',showNewContent);
-		};
-
-		function showNewContent() {
-			$('#contentId').show('normal');
-		}
-		return false;
-	});
+    $("#contentId").load("about.html");
 });
 
 
-
-
-
-
-changeContent('About');
+// load_page('about.html');
